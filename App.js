574561d93b45676b21e39d98b86bcc9ee503b929
gmahima <mahimagangavarapu@gmail.com>
2020-02-21 22:57:04 +0530

    first rnative app

---
+++ App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Text, View} from 'react-native'
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+        <Text>
+          Hello World
+        </Text>
+      </View>
+    )
+  }
+}
