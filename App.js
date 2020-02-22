5fc109c8be0bc16b6de41851b3661177898d025d
gmahima <mahimagangavarapu@gmail.com>
2020-02-22 12:04:59 +0530

    add img

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react'
-import {Text, View} from 'react-native'
+import {Text, View, Image} from 'react-native'
 
 export default class App extends Component{
   render() {
+    let pic = {
+      uri:
+    }
     return (
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text>
-          Hello World
-        </Text>
-      </View>
+        <Image source={pic} style={{width: 193, height: 110}}/>
+
     )
   }
-}
+ }
+// import React, { Component } from 'react';
+// import { Image } from 'react-native';
+
+// export default class Bananas extends Component {
+//   render() {
+//     let pic = {
+//       uri: 
+//     };
+//     return (
+//       <Image source={pic} style={{width: 193, height: 110}}/>
+//     );
+//   }
+// }
